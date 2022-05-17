98416c3b0020568e960b7a4df905953fa375b2f1
Nicola Field <nicola.field2@gmail.com>
2022-05-17 12:43:20 +0100

    Adding keyboard input and tidying up code

--- script.js
+++ script.js
@@ -2,9 +2,51 @@ let userInput = 'addition';
 let num1 = 0;
 let num2 = 0;
 let result = 0;
+let displayValue = 0;
+let isEqualsPushed = false;
+let isOperatorPushed = false;
+let numberValue = 0;
+
 const calculatorContainer = document.querySelector('#calculator-container');
 const infinityQuote = document.querySelector('#infinity-quote'); 
 const refreshButton = document.querySelector('#refresh');
+const display = document.querySelector('#display');
+const runningTotal = document.querySelector('#running-total');
+const numberButtons = document.querySelectorAll('.numbers');
+const decimalButton = document.querySelector('.decimal');
+const additionButton = document.querySelector('#addition');
+const divisionButton = document.querySelector('#division');
+const multiplicationButton = document.querySelector('#multiplication');
+const subtractionButton = document.querySelector('#subtraction');
+const equalsButton = document.querySelector('#equals');
+const clearButton = document.querySelector('#clear-button');
+const backspaceButton = document.querySelector('#backspace-button');
+
+decimalButton.addEventListener('click', addDecimalPointToDisplay);
+equalsButton.addEventListener('click', getEqualsOperatorResult);
+clearButton.addEventListener('click', clear);
+backspaceButton.addEventListener('click', backspace);
+additionButton.addEventListener('click', getAdditionOperatorResult);
+subtractionButton.addEventListener('click', getSubtractionOperatorResult);
+multiplicationButton.addEventListener('click', getMultiplicationOperatorResult);
+divisionButton.addEventListener('click', getDivisionOperatorResult);
+equalsButton.addEventListener('click', getEqualsOperatorResult);
+window.addEventListener('keydown', getKeyboardInput);
+
+function getKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) {
+        numberValue = e.key;
+        addNumbersToDisplay();
+    }
+    if (e.key === '.') addDecimalPointToDisplay();
+    if (e.key === '=' || e.key === 'Enter') getEqualsOperatorResult();
+    if (e.key === 'Backspace') backspace();
+    if (e.key === 'Escape') clear();
+    if (e.key === '+') getAdditionOperatorResult();
+    if (e.key === '-') getSubtractionOperatorResult();
+    if (e.key === '*') getMultiplicationOperatorResult();
+    if (e.key === '/') getDivisionOperatorResult();
+}
 
 function add(a, b) {
     return a+b;
@@ -46,60 +88,51 @@ function operate(userInput, num1, num2) {
         num1 = Number(num1);
         num2 = Number(num2);
         result = divide(num1, num2);
-    } else console.log('something went wrong');
+    } else return;
 }
-
-
-
-const display = document.querySelector('#display');
-const runningTotal = document.querySelector('#running-total');
-const numberButtons = document.querySelectorAll('.numbers');
-
-let displayValue = 0;
-let isEqualsPushed = false;
 
 numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click', () => {
-        addEventListeners();
-        if(isEqualsPushed == true) {
-            num1 = 0;
-            num2 = 0;
-            userInput = 'addition';
-            result = 0;
-            display.innerHTML = '0';
-            runningTotal.innerHTML = 0;
-            runningTotal.style.visibility = 'visible';
-            isEqualsPushed = false;
-        }
-        let numberValue = numberButton.innerHTML;
-        if(display.innerHTML == '0') {
-            display.innerHTML = numberValue
-            displayValue = display.innerHTML;
-        } else if(display.innerHTML.length > 25) {
-            console.log('hi');
-        } 
-        else {
-            display.innerHTML += numberValue;
-            displayValue = display.innerHTML; 
-        }
+        numberValue = numberButton.innerHTML;
+        addNumbersToDisplay();
     })
 })
 
-const decimalButton = document.querySelector('.decimal');
-decimalButton.addEventListener('click', getDecimalPoint);
-
-function getDecimalPoint (e) {
-    display.innerHTML += '.';
-    displayValue = display.innerHTML;
-    removeEventListeners();
-    decimalButton.removeEventListener('click', getDecimalPoint);
+function addNumbersToDisplay() {
+    isOperatorPushed = false;
+    if(isEqualsPushed == true) {
+        display.innerHTML += numberValue;
+        displayValue = display.innerHTML; 
+        userInput = 'addition';
+        result = 0;
+        runningTotal.innerHTML = 0;
+        runningTotal.style.visibility = 'visible';
+        isEqualsPushed = false;
+    } else if(display.innerHTML == '0') {
+        display.innerHTML = numberValue
+        displayValue = display.innerHTML;
+    } else if(display.innerHTML.length > 25) {
+        return;
+    } 
+    else {
+        display.innerHTML += numberValue;
+        displayValue = display.innerHTML; 
+    }
 }
 
-
-const additionButton = document.querySelector('#addition');
-const divisionButton = document.querySelector('#division');
-const multiplicationButton = document.querySelector('#multiplication');
-const subtractionButton = document.querySelector('#subtraction');
+function addDecimalPointToDisplay (e) {
+    if (isEqualsPushed == true) {
+        result = 0;
+        isEqualsPushed = false;
+    }
+    let displayString = display.innerHTML;
+    if (displayString.includes('.') == true) {
+        return;
+    } else {
+        display.innerHTML += '.';
+        displayValue = display.innerHTML;
+    }
+}
 
 function getOperatorResult() {
     num1 = result;
@@ -107,90 +140,78 @@ function getOperatorResult() {
     operate(userInput, num1, num2);
     display.innerHTML = '';
     runningTotal.style.visibility = 'visible';
-    decimalButton.addEventListener('click', getDecimalPoint);
-    isThereDecimalPoint = false;
-    isOperatorPressed = false;
+    isOperatorPushed = true;
 }
 
 function getAdditionOperatorResult(e) {
-    getOperatorResult();
-    userInput = 'addition';
-    runningTotal.innerHTML = `${result} + `;
-    isEqualsPushed = false;
-    removeEventListeners();
+    if (isOperatorPushed == false) {
+        getOperatorResult();
+        userInput = 'addition';
+        runningTotal.innerHTML = `${result} + `;
+        isEqualsPushed = false;
+    } else {
+        return;
+    }
 }
 
 function getSubtractionOperatorResult(e) {
-    getOperatorResult();
-    userInput = 'subtraction';
-    runningTotal.innerHTML = `${result} - `;
-    isEqualsPushed = false;
-    removeEventListeners();
+    if (isOperatorPushed == false) {
+        getOperatorResult();
+        userInput = 'subtraction';
+        runningTotal.innerHTML = `${result} - `;
+        isEqualsPushed = false;
+    } else {
+        return;
+    }
 }
 
 function getMultiplicationOperatorResult(e) {
-    getOperatorResult();
-    userInput = 'multiplication';
-    runningTotal.innerHTML = `${result} x `;
-    isEqualsPushed = false;
-    removeEventListeners();
+    if (isOperatorPushed == false) {
+        getOperatorResult();
+        userInput = 'multiplication';
+        runningTotal.innerHTML = `${result} x `;
+        isEqualsPushed = false;
+    } else {
+        return;
+    }
 }
 
 function getDivisionOperatorResult(e) {
-    getOperatorResult();
-    userInput = 'division';
-    runningTotal.innerHTML = `${result} &#247 `;
-    isEqualsPushed = false;
-    removeEventListeners();
+    if (isOperatorPushed == false) {
+        getOperatorResult();
+        userInput = 'division';
+        runningTotal.innerHTML = `${result} &#247 `;
+        isEqualsPushed = false;
+    } else {
+        return;
+    }
 }
-const equalsButton = document.querySelector('#equals');
-
-function addEventListeners() {
-    additionButton.addEventListener('click', getAdditionOperatorResult);
-    subtractionButton.addEventListener('click', getSubtractionOperatorResult);
-    multiplicationButton.addEventListener('click', getMultiplicationOperatorResult);
-    divisionButton.addEventListener('click', getDivisionOperatorResult);
-    equalsButton.addEventListener('click', getEqualsOperatorResult);
-}
-
-addEventListeners();
-
-function removeEventListeners() {
-    additionButton.removeEventListener('click', getAdditionOperatorResult);
-    subtractionButton.removeEventListener('click', getSubtractionOperatorResult);
-    multiplicationButton.removeEventListener('click', getMultiplicationOperatorResult);
-    divisionButton.removeEventListener('click', getDivisionOperatorResult);
-    equalsButton.removeEventListener('click', getEqualsOperatorResult);
-}
-
-
-
-
-equalsButton.addEventListener('click', getEqualsOperatorResult);
 
 function getEqualsOperatorResult(e) {
-    getOperatorResult();
+    if (isOperatorPushed == false) getOperatorResult();
     display.innerHTML = result;
     runningTotal.style.visibility = 'hidden';
     num2 = 0;
     displayValue = 0;
     userInput = 'addition';
     isEqualsPushed = true; 
+    isOperatorPushed = false;
 }
 
-const clearButton = document.querySelector('#clear-button');
-clearButton.addEventListener('click', () => {
+function clear(e) {
     num1 = 0;
     num2 = 0;
     userInput = 'addition';
     result = 0;
+    displayValue = 0;
     display.innerHTML = '0';
     runningTotal.innerHTML = 0;
     runningTotal.style.visibility = 'visible';
-})
+    isEqualsPushed = false;
+    isOperatorPushed = false;
+}
 
-const backspaceButton = document.querySelector('#backspace-button');
-backspaceButton.addEventListener('click', () => {
+function backspace(e) {
     if (display.innerHTML == result) {
         let numString = display.innerHTML;
         numString = numString.toString();
@@ -198,125 +219,14 @@ backspaceButton.addEventListener('click', () => {
         display.innerHTML = numString;
         displayValue = display.innerHTML;
         result = 0;
+        isEqualsPushed = false;
         
     } else {
         let numString = display.innerHTML;
         numString = numString.toString();
         const numStringLength = numString.length;
-        if(numString.charAt(numStringLength - 1) == '.') {
-            decimalButton.addEventListener('click', getDecimalPoint);
-        }
         numString = numString.slice(0, -1);
         display.innerHTML = numString;
         displayValue = display.innerHTML;
-    }
-
-})
-
-window.addEventListener('keydown', getKeyboardInput);
-let isThereDecimalPoint = false;
-let isOperatorPressed = false;
-
-function getKeyboardInput(e) {
-    if (e.key >= 0 && e.key <= 9) {
-        addEventListeners();
-        isOperatorPressed = false;
-        if(isEqualsPushed == true) {
-            num1 = 0;
-            num2 = 0;
-            userInput = 'addition';
-            result = 0;
-            display.innerHTML = '0';
-            runningTotal.innerHTML = 0;
-            runningTotal.style.visibility = 'visible';
-            isEqualsPushed = false;
-        }
-        let numberValue = e.key;
-        if(display.innerHTML == '0') {
-            display.innerHTML = numberValue
-            displayValue = display.innerHTML;
-        } else if(display.innerHTML.length > 25) {
-            console.log('hi');
-        } 
-        else {
-            display.innerHTML += numberValue;
-            displayValue = display.innerHTML; 
-        }
-    }
-    if (e.key === '.') {
-        if(isThereDecimalPoint == true) {
-            console.log('hi');
-        } else {
-        getDecimalPoint();
-        isThereDecimalPoint = true;
-        isOperatorPressed = false;
-        }
-    }
-    if (e.key === '=' || e.key === 'Enter') {
-        if (isOperatorPressed == true) {
-            console.log('hi')
-        } else getEqualsOperatorResult();
-    }
-    if (e.key === 'Backspace') {
-        if (display.innerHTML == result) {
-            let numString = display.innerHTML;
-            numString = numString.toString();
-            numString = numString.slice(0, -1);
-            display.innerHTML = numString;
-            displayValue = display.innerHTML;
-            result = 0;
-            
-        } else {
-            let numString = display.innerHTML;
-            numString = numString.toString();
-            const numStringLength = numString.length;
-            if(numString.charAt(numStringLength - 1) == '.') {
-                decimalButton.addEventListener('click', getDecimalPoint);
-            }
-            numString = numString.slice(0, -1);
-            display.innerHTML = numString;
-            displayValue = display.innerHTML;
-        }
-    }
-    if (e.key === 'Escape') {
-        num1 = 0;
-        num2 = 0;
-        userInput = 'addition';
-        result = 0;
-        display.innerHTML = '0';
-        runningTotal.innerHTML = 0;
-        runningTotal.style.visibility = 'visible';
-    }
-    if (e.key === '+') {
-        if (isOperatorPressed == true) {
-            console.log('hi')
-        } else {
-            getAdditionOperatorResult();
-            isOperatorPressed = true;
-        }
-    }
-    if (e.key === '-') {
-        if (isOperatorPressed == true) {
-            console.log('hi')
-        } else {
-            getSubtractionOperatorResult();
-            isOperatorPressed = true;
-        }
-    }
-    if (e.key === '*') {
-        if (isOperatorPressed == true) {
-            console.log('hi')
-        } else {
-            getMultiplicationOperatorResult();
-            isOperatorPressed = true;
-        }
-    }
-    if (e.key === '/') {
-        if (isOperatorPressed == true) {
-            console.log('hi')
-        } else {
-            getDivisionOperatorResult();
-            isOperatorPressed = true;
-        }
     }
 }
